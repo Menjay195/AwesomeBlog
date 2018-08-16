9819183f3a4e0d6660de41302b4a6985b7c379ff
Menjay195 <i_love_thu@163.com>
2018-08-16 19:30:58 +0800

    MVC,created in day07

---
+++ www/handlers.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from webframe import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+@asyncio.coroutine
+def index(request):
+    users = yield from User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
